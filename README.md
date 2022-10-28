d6ab471327400ee786fb41a7b1117cc93c6427e0
Your Name <you@example.com>
2022-10-28 17:31:56 +0800

    commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# arm_cassie_env
+Mujoco210 is required for simulation and should be placed in ~/.mujoco . To build the mujoco simulation, `make` in `./cassie-mujoco-sim` folder. Then copy  `libcassiemujoco.so` to arm-cassie/arm_cassie_env/cassie_m
+
+Stable-baselines3 in required for rl algorithm. Create conda env:
+
+```
+conda env create -f environment.yml
+conda activate lg
+```
+
+Run `python train.py` to train the agent defined in cassieRLEnvStable.py.
+
+Run `python play.py` to test the policy.
+
+Run `python arm.py` to run the task.
+
+XML Models for possible use are in `./models`
---
+++ modelPool/light/meshes/bulletCA.py
@@ -0,0 +1,26 @@
+import time
+
+import pybullet
+import pybullet_data
+
+
+if __name__ == '__main__':
+    client = pybullet.connect(pybullet.GUI)
+    pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_TINY_RENDERER, 0)
+
+    pybullet.setGravity(0, 0, -9.8)
+    pybullet.setRealTimeSimulation(1)
+
+    # 载入urdf格式是场景
+    pybullet.loadURDF("plane100.urdf", useMaximalCoordinates=True)
+    # 载入urdf格式的机器人
+    r_ind = pybullet.loadURDF('GEN3-LITE.urdf', (3, 1, 1), pybullet.getQuaternionFromEuler([0, 0, 0.785]))
+
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RENDERING, 1)
+    while True:
+        time.sleep(1. / 240.)
+
+
